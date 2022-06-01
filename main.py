4dfca38c0a7a1e2581332bc2684d91f6600b1eb3
Ehtisham Sabir <Shaamdbz@gmail.com>
2022-06-01 12:58:36 +0500

    updated

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import os
+import re
+
 from flask import Flask, render_template, request, make_response, flash, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SUPER SECRET'
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def home():
     username = request.cookies.get('username')
     if username:
         return render_template('home.html', username=username)
     return render_template('home.html')
 
-@app.route('/login', methods = ['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     username = request.cookies.get('username')
     if username:
         return render_template('login.html', username=username)
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username=='admin' and password=='admin':
+        if username == 'admin' and password == 'admin':
             flash("Successful login", "success")
             resp = make_response(redirect('/'))
             resp.set_cookie('username', username)
@@ -27,9 +32,25 @@ def login():
             flash("Wrong username or password", "danger")
     return render_template('login.html')
 
-@app.route('/logout', methods = ['GET'])
+
+@app.route('/logout', methods=['GET'])
 def logout():
     resp = make_response(redirect('/'))
     resp.delete_cookie('username')
     return resp
-app.run(host='0.0.0.0', port=80,debug=True)
+
+
+@app.route('/devices', methods=['GET'])
+def devices(test=True):
+    if test:
+        return [{'IP': '_gateway', 'LAN_IP': '10.255.255.254', 'MAC_ADDRESS': '00:10:db:ff:10:01'},
+                {'IP': '?', 'LAN_IP': '172.18.0.2', 'MAC_ADDRESS': '02:42:ac:12:00:02'},
+                {'IP': '?', 'LAN_IP': '10.255.255.101', 'MAC_ADDRESS': '70:4c:a5:81:38:78'}]
+    full_results = [re.findall('^[\w\?\.]+|(?<=\s)\([\d\.]+\)|(?<=at\s)[\w\:]+', i) for i in os.popen('arp -a')]
+    final_results = [dict(zip(['IP', 'LAN_IP', 'MAC_ADDRESS'], i)) for i in full_results]
+    final_results = [{**i, **{'LAN_IP': i['LAN_IP'][1:-1]}} for i in final_results]
+
+    return final_results
+
+
+app.run(host='0.0.0.0', port=80, debug=True)
