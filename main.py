e27ebb4d3f9de49016e0567b58a259d3bddb83fa
Ehtisham Sabir <Shaamdbz@gmail.com>
2022-06-05 16:17:45 +0500

    live capture support added

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def cap_file_queue():
     try:
         filenames = request.get_json(force=True)['files']
     except:
-        return {"queue": files_in_queue}
+        return {"queue": files_in_queue, "processing": len(processing), "processed": processed_files}
     print(filenames)
     if filenames is None:
         print("it's none")
